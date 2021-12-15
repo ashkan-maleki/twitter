03edb35073b1ae59400501d7377f2146d1df3577
Maleki <malekiismc@gmail.com>
2021-12-15 14:32:41 +0330

    swagger route is root now.

--- src/Application/Api/Program.cs
+++ src/Application/Api/Program.cs
@@ -7,10 +7,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/message", () => "Hello World!");
 
 
 
