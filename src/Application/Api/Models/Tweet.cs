7386761c80325f0c0c75908c1528ca0f1cfc239d
Maleki <malekiismc@gmail.com>
2021-12-16 14:04:49 +0330

    first ef entities and context setup

---
+++ src/Application/Api/Models/Tweet.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models
+{
+    public class Tweet
+    {
+        public int TweetId { get; set; }
+        public string? Text { get; set; }
+        
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
