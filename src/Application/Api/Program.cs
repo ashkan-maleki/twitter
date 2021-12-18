910c8a8747ced6bfe7b11f31599c5415dc9a4401
mamalmaleki <johnnywcage@outlook.com>
2021-12-18 21:33:13 +0330

    connection string changed

--- src/Application/Api/Program.cs
+++ src/Application/Api/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TwitterContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TwitterContext")));
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
